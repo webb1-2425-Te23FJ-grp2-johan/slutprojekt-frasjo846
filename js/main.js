1147a7a8987320be4363f05a98cc351a78f6bf05
frasjo846 <frasjo846@edu.linkoping.se>
2025-04-08 09:02:27 +0000

    Pregress, add footer, grid

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const menuTitle = document.getElementById("menu-content");
 const navMenu = document.getElementById("nav-menu");
 const dropdown = document.getElementById("content-drop-down");
 
-menuTitle.addEventListener("click", () => {
+menuTitle.addEventListener("click", function() {
   navMenu.classList.toggle("hidden");
-  dropdown.classList.toggle("arrowmove");
+  dropdown.classList.toggle("invertscale");
 });
