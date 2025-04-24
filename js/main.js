126666b9fe501d5a518f9efede0b6da9951bbc0b
frasjo846 <frasjo846@edu.linkoping.se>
2025-04-24 15:32:24 +0000

    Fixed better navbar for pc, fixed lightbox

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-const menuTitle = document.getElementById("menu-content");
+const navTitle = document.getElementById("nav-Title");
 const navMenu = document.getElementById("nav-menu");
 const dropdown = document.getElementById("content-drop-down");
+const NavRebuildWith = 680;
 
-menuTitle.addEventListener("click", function() {
+navTitle.addEventListener("click", function() {
   navMenu.classList.toggle("hidden");
   dropdown.classList.toggle("invertscale");
 });
+
+
+checkScreenWidth();
+function checkScreenWidth() {
+  if (window.innerWidth > NavRebuildWith) {
+    navTitle.classList.add("hidden"); // Use Tailwind's hidden class
+    navMenu.classList.remove("hidden");
+  } else {
+    navTitle.classList.remove("hidden"); // Ensure it's visible on small screens
+  }
+}
+ //check on page load
